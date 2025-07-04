65b001788c3130673b53192ba8cb09dd1db0e325
williamloosman <william.loosman@kadaster.nl>
2025-07-04 23:13:00 +0200

    inspire

--- pkg/repository/inspire.go
+++ pkg/repository/inspire.go
@@ -24,32 +24,26 @@ func (hvd *InspireRepository) Download(kind model.InspireRegisterKind) error {
 	return nil
 }
 
-func (hvd *InspireRepository) getThemes() ([]model.InspireTheme, error) {
-	// todo: Check if file exists in localCachePath
+func (hvd *InspireRepository) getKind() ([]interface{}, error) {
+	// todo: make this func generic so
+	//  - when we ask model.InspireTheme we Download model.InspireKindTheme and parse english and dutch results into []model.InspireTheme and return this
+	//  - when we ask model.InspireLayer we Download model.InspireKindLayer and parse english and dutch results into []model.InspireLayer and return this
+
+	// todo: for every file check if it exists in localCachePath
 	// todo: If not, download it
 	// todo: If file exists, check if it is older than 3 days -> If so, download it
 	// todo: If file exists and is not older than 3 days, read it
-	// todo: Parse the file and return the result as []model.InspireTheme
+	// todo: Parse the file and return the result as []T
 
 	return nil, nil
 }
 
-func (hvd *InspireRepository) getLayers() ([]model.InspireLayer, error) {
-	// todo: Check if file exists in localCachePath
-	// todo: If not, download it
-	// todo: If file exists, check if it is older than 3 days -> If so, download it
-	// todo: If file exists and is not older than 3 days, read it
-	// todo: Parse the file and return the result as []model.InspireLayer
-
-	return nil, nil
-}
-
-func (hvd *InspireRepository) GetAllThemes() ([]model.InspireTheme, error) {
+func (hvd *InspireRepository) GetAllThemes() ([]interface{}, error) {
 	// Call getThemes to retrieve all INSPIRE themes
-	return hvd.getThemes()
+	return hvd.getKind()
 }
 
-func (hvd *InspireRepository) GetAllLayers() ([]model.InspireLayer, error) {
+func (hvd *InspireRepository) GetAllLayers() ([]interface{}, error) {
 	// Call getLayers to retrieve all INSPIRE layers
-	return hvd.getLayers()
+	return hvd.getKind()
 }
