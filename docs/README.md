f830ec190efd3555dfaa70dc8ba5b1044633318b
williamloosman <william.loosman@kadaster.nl>
2025-07-07 09:46:50 +0200

    inspire

--- docs/README.md
+++ docs/README.md
@@ -32,7 +32,7 @@ Shows example of <input_file_service_specifics> for users that are not familiar 
 
 Used to retrieve and inspect high value dataset categories from the HVD Thesaurus.
 
-**--local-path**="": Local path where the HVD Thesaurus is cached. (default: ./high-value-dataset-category.rdf)
+**--local-path**="": Local path where the HVD Thesaurus is cached. (default: ./cache/high-value-dataset-category.rdf)
 
 **--url**="": HVD Thesaurus endpoint which should contain the HVD categories as RDF format. (default: https://op.europa.eu/o/opportal-service/euvoc-download-handler?cellarURI=http%3A%2F%2Fpublications.europa.eu%2Fresource%2Fdistribution%2Fhigh-value-dataset-category%2F20241002-0%2Frdf%2Fskos_core%2Fhigh-value-dataset-category.rdf&fileName=high-value-dataset-category.rdf)
 
@@ -43,6 +43,12 @@ Downloads the RDF Thesaurus containing the HVD categories at local-path.
 ### list
 
 Displays list of HVD categories.
+
+### csv
+
+Exports HVD categories to a CSV file.
+
+**-o**="": Output file path for the CSV file. (default: ./cache/high-value-dataset-category.csv)
 
 ## inspire
 
--- internal/app/inspire.go
+++ internal/app/inspire.go
@@ -17,15 +17,40 @@ func init() {
 				Name:  "list",
 				Usage: "List inspire themes or layers. Usage: pmt inspire list <themes|layers>",
 				Action: func(ctx context.Context, cmd *cli.Command) error {
+					if cmd.NArg() == 0 {
+						return fmt.Errorf("please specify a register kind")
+					}
 
-					repo := repository.NewInspireRepository(InspireLocalPath)
-
-					layers, err := repo.GetAllLayers()
-					if err != nil {
-						return err
+					if cmd.Args().First() != "theme" && cmd.Args().First() != "layer" {
+						return fmt.Errorf("please specify a register kind")
 					}
 
 					fmt.Println("list inspire: ", cmd.Args().First())
+
+					repo := repository.NewInspireRepository(InspireLocalPath)
+
+					if cmd.Args().First() == "theme" {
+						themes, err := repo.GetThemes()
+						if err != nil {
+							return err
+						}
+
+						for _, theme := range themes {
+							fmt.Println(theme.Id + "-" + theme.LabelDutch)
+						}
+					}
+
+					if cmd.Args().First() == "layer" {
+						layers, err := repo.GetLayers()
+						if err != nil {
+							return err
+						}
+
+						for _, layer := range layers {
+							fmt.Println(layer.Id + "-" + layer.LabelDutch)
+						}
+					}
+
 					return nil
 				},
 				ShellComplete: func(ctx context.Context, cmd *cli.Command) {
