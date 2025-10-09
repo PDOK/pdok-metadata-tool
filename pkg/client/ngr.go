6858b109ea1eb3944351a94f9695cadfb30a9b70
rahmab <basim.rahmat-alla@kadaster.nl>
2025-10-09 10:59:26 +0200

    PDOK-18218 PDOK Metadata tool - Store uitwerken

--- pkg/client/ngr.go
+++ pkg/client/ngr.go
@@ -1,45 +1,282 @@
 package client
 
 import (
+	"bytes"
+	"encoding/base64"
+	"errors"
 	"fmt"
+	log "github.com/sirupsen/logrus"
+	"io"
 	"net/http"
-	"net/url"
+	"strings"
 	"time"
 
 	"github.com/pdok/pdok-metadata-tool/pkg/model/ngr"
 )
 
-// NgrClient is used as a client for doing NGR requests.
 type NgrClient struct {
-	host   *url.URL
-	client *http.Client
+	NgrClient *http.Client
+	NgrConfig NgrConfig
 }
 
-// NewNgrClient creates a new instance of NgrClient.
-func NewNgrClient(host *url.URL) NgrClient {
-	const defaultTimeoutSeconds = 20
+type NgrConfig struct {
+	NgrUrl      string
+	NgrUserName string
+	NgrPassword string
+}
 
+const API_RECORDS_TEMPlATE = "/geonetwork/srv/api/records"
+const API_LOGIN_PART = "/geonetwork/srv/dut/info?type=me"
+
+func NewNgrClient(config NgrConfig) NgrClient {
 	client := &http.Client{
-		Timeout: defaultTimeoutSeconds * time.Second,
+		Timeout: 20 * time.Second,
 	}
-
 	return NgrClient{
-		host:   host,
-		client: client,
+		NgrConfig: config,
+		NgrClient: client,
 	}
 }
 
-// GetRecordTags returns the NGR tags for a give record id.
-// TODO Use this for harvesting only INSPIRE service metadata in ETF-validator-go.
+// TODO Use this for harvesting only INSPIRE service metadata in ETF-validator-go
 func (c NgrClient) GetRecordTags(uuid string) (ngr.RecordTagsResponse, error) {
-	mdTagURL := fmt.Sprintf("%s/geonetwork/srv/api/records/%s/tags", c.host.String(), uuid)
+	mdTagUrl := fmt.Sprintf("%s/geonetwork/srv/api/records/%s/tags", c.NgrConfig.NgrUrl, uuid)
 
 	recordTagsResponse := ngr.RecordTagsResponse{}
 
-	err := getUnmarshalledJSONResponse(&recordTagsResponse, mdTagURL, *c.client)
+	err := getUnmarshalledJSONResponse(&recordTagsResponse, mdTagUrl, *c.NgrClient)
 	if err != nil {
 		return nil, err
 	}
-
 	return recordTagsResponse, nil
+}
+
+// TODO: UNDER DEVELOPEMENT
+
+func obtainXSRFToken(ngrConfig *NgrConfig) (string, error) {
+	// Build URL
+	url := ngrConfig.NgrUrl + API_LOGIN_PART
+
+	// Create HTTP client and request
+	req, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
+		return "", fmt.Errorf("cannot create request: %w", err)
+	}
+
+	// Set Basic Auth
+	username := ngrConfig.NgrUserName
+	password := ngrConfig.NgrPassword
+	req.SetBasicAuth(username, password)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("error executing request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// In Kotlin, success (200) is considered wrong and should throw an error
+	if resp.StatusCode < 400 {
+		return "", errors.New("cannot obtain XSRF token, server shouldn't allow POST action")
+	}
+
+	// Look for 403 Forbidden
+	if resp.StatusCode == http.StatusForbidden {
+		// Parse cookies from headers
+		cookies := resp.Header.Values("Set-Cookie")
+		for _, c := range cookies {
+			if strings.Contains(c, "XSRF-TOKEN=") {
+				parts := strings.Split(c, ";")
+				for _, part := range parts {
+					part = strings.TrimSpace(part)
+					if strings.HasPrefix(part, "XSRF-TOKEN=") {
+						split := strings.SplitN(part, "=", 2)
+						if len(split) == 2 {
+							return split[1], nil
+						}
+					}
+				}
+			}
+		}
+		return "", errors.New("cannot obtain XSRF token from cookie")
+	}
+
+	return "", fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+}
+
+func (c *NgrClient) createOrUpdateServiceMetadataRecord(
+	record string,
+	uuid string,
+	categoryId *string,
+	groupId *string,
+	ngrConfig *NgrConfig,
+	toBePublished bool,
+) error {
+	// Build URL with query params
+	params := "?metadataType=METADATA&uuidProcessing=REMOVE_AND_REPLACE"
+	if toBePublished {
+		params += "&publishToAll=true"
+	} else {
+		params += "&publishToAll=false"
+	}
+	if groupId != nil {
+		params += "&group=" + *groupId
+	}
+	if categoryId != nil {
+		params += "&category=" + *categoryId
+	}
+
+	url := fmt.Sprintf(
+		"%s%s/%s",
+		c.NgrConfig.NgrUrl,
+		API_RECORDS_TEMPlATE,
+		params,
+	)
+
+	// PUT the record
+	putReq, err := http.NewRequest(http.MethodPut, url, bytes.NewBufferString(record))
+	if err != nil {
+		return fmt.Errorf("failed to create PUT request: %w", err)
+	}
+	headers, err := c.getHeaders(ngrConfig)
+	if err != nil {
+		return fmt.Errorf("failed to create headers request: %w", err)
+	}
+	putReq.Header = headers
+	putResp, err := c.NgrClient.Do(putReq)
+	if err != nil {
+		return fmt.Errorf("PUT request error: %w", err)
+	}
+	defer putResp.Body.Close()
+	body, _ := io.ReadAll(putResp.Body)
+	if putResp.StatusCode < 200 || putResp.StatusCode >= 300 {
+		return fmt.Errorf(
+			"PUT request failed with status %d: %s",
+			putResp.StatusCode,
+			string(body),
+		)
+	}
+	return nil
+}
+
+func (c *NgrClient) getRecord(uuid string, ngrConfig *NgrConfig) (string, int, error) {
+	// 1. Check if record is already published
+	ngrUrl := fmt.Sprintf("%s%s/%s",
+		c.NgrConfig.NgrUrl,
+		API_RECORDS_TEMPlATE,
+		uuid,
+	)
+	bodyString := ""
+
+	getReq, err := http.NewRequest(http.MethodGet, ngrUrl, nil)
+
+	headers, err := c.getHeaders(ngrConfig)
+	if err != nil {
+		return "", http.StatusInternalServerError, fmt.Errorf("failed to create headers request: %w", err)
+	}
+	getReq.Header = headers
+
+	if err != nil {
+		return "", http.StatusInternalServerError, fmt.Errorf("failed to make http request: %w", err)
+	}
+	getResp, err := c.NgrClient.Do(getReq)
+	if err != nil {
+		return "", http.StatusInternalServerError, fmt.Errorf("failed to send http request: %w", err)
+	}
+	if err == nil {
+		defer getResp.Body.Close()
+		switch getResp.StatusCode {
+		case http.StatusOK:
+			bodyBytes, err := io.ReadAll(getResp.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			bodyString = string(bodyBytes)
+			return bodyString, http.StatusOK, nil
+		case http.StatusNotFound:
+			// ok, record does not exist
+		case http.StatusForbidden:
+			// ok, record is unpublished
+		default:
+			return bodyString, getResp.StatusCode, fmt.Errorf(
+				"unexpected http status %d retrieving sharing info for record %s",
+				getResp.StatusCode,
+				uuid,
+			)
+		}
+	}
+	return bodyString, getResp.StatusCode, fmt.Errorf(
+		"unexpected http status %d retrieving sharing info for record %s",
+		getResp.StatusCode,
+		uuid,
+	)
+}
+
+func (c *NgrClient) deleteRecord(uuid string, ngrConfig *NgrConfig) (int, error) {
+	// 1. Check if record is already published
+	ngrUrl := fmt.Sprintf("%s%s/%s",
+		c.NgrConfig.NgrUrl,
+		API_RECORDS_TEMPlATE,
+		uuid,
+	)
+
+	deleteReq, err := http.NewRequest(http.MethodDelete, ngrUrl, nil)
+
+	headers, err := c.getHeaders(ngrConfig)
+	if err != nil {
+		return http.StatusInternalServerError, fmt.Errorf("failed to create headers request: %w", err)
+	}
+	deleteReq.Header = headers
+
+	if err != nil {
+		return http.StatusInternalServerError, fmt.Errorf("failed to make http request: %w", err)
+	}
+	deleteResp, err := c.NgrClient.Do(deleteReq)
+	if err != nil {
+		return http.StatusInternalServerError, fmt.Errorf("failed to send http request: %w", err)
+	}
+	if err == nil {
+		defer deleteResp.Body.Close()
+		switch deleteResp.StatusCode {
+		case http.StatusNoContent:
+			return http.StatusNoContent, nil
+		case http.StatusNotFound:
+			// ok, record does not exist
+		case http.StatusForbidden:
+			// ok, record is unpublished
+		default:
+			return deleteResp.StatusCode, fmt.Errorf(
+				"unexpected http status %d retrieving sharing info for record %s",
+				deleteResp.StatusCode,
+				uuid,
+			)
+		}
+	}
+	return deleteResp.StatusCode, fmt.Errorf(
+		"unexpected http status %d retrieving sharing info for record %s",
+		deleteResp.StatusCode,
+		uuid,
+	)
+}
+
+func (c *NgrClient) getHeaders(ngrConfig *NgrConfig) (http.Header, error) {
+	xsrfToken, err := obtainXSRFToken(ngrConfig)
+	if err != nil {
+		return nil, fmt.Errorf("failed to obtain XSRF token: %w", err)
+	}
+
+	// In the old selfservice was a check to block update for published records.
+	// In the new selfservice we need to update the record regardless of publish state
+
+	username := c.NgrConfig.NgrUserName
+	password := c.NgrConfig.NgrPassword
+	auth := base64.StdEncoding.EncodeToString([]byte(username + ":" + password))
+
+	headers := http.Header{}
+	headers.Set("Authorization", "Basic "+auth)
+	headers.Set("X-XSRF-TOKEN", xsrfToken)
+	headers.Set("Cookie", "XSRF-TOKEN="+xsrfToken)
+	headers.Set("Content-Type", "application/xml")
+	headers.Set("Accept", "application/json, application/xml")
+	return headers, nil
 }
--- pkg/client/ngr_test.go
+++ pkg/client/ngr_test.go
@@ -1,13 +1,15 @@
 package client
 
 import (
-	"net/http/httptest"
-	"net/url"
-	"testing"
-
 	"github.com/pdok/pdok-metadata-tool/pkg/model/ngr"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
+	"strings"
+	"testing"
+	"time"
 )
 
 func TestNgrClient_GetRecordTags(t *testing.T) {
@@ -17,7 +19,6 @@ func TestNgrClient_GetRecordTags(t *testing.T) {
 	type args struct {
 		uuid string
 	}
-
 	tests := []struct {
 		name         string
 		args         args
@@ -56,7 +57,7 @@ func TestNgrClient_GetRecordTags(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := ngrClient.GetRecordTags(tt.args.uuid)
 
-			require.NoError(t, err)
+			assert.Nil(t, err)
 			assert.Len(t, got, tt.wantNrOfTags)
 			assert.Equal(t, tt.want, got)
 		})
@@ -64,14 +65,84 @@ func TestNgrClient_GetRecordTags(t *testing.T) {
 }
 
 func getNgrClient(t *testing.T, mockedNGRServer *httptest.Server) *NgrClient {
-	t.Helper()
-
-	hostURL, err := url.Parse(mockedNGRServer.URL)
+	//hostURL, err := url.Parse(mockedNGRServer.URL)
+	accUrl, err := url.Parse("https://ngr.acceptatie.nationaalgeoregister.nl")
 	if err != nil {
 		t.Fatalf("Failed to parse mocked NGR server URL: %v", err)
 	}
 
-	ngrClient := NewNgrClient(hostURL)
-
+	config := NgrConfig{
+		NgrUrl:      accUrl.String(),
+		NgrUserName: "TESTME",
+		NgrPassword: "TESTME",
+	}
+	ngrClient := NewNgrClient(config)
 	return &ngrClient
+}
+
+func TestNgrClient_updateServiceMetadataRecord(t *testing.T) {
+	//categoryId := "224342"
+	mockedNGRServer := preTestSetup()
+	ngrClient := getNgrClient(t, mockedNGRServer)
+
+	type args struct {
+		pathRecordXml string
+		uuid          string
+		categoryId    *string
+		groupId       *string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name:    "test_01",
+			wantErr: false,
+			args: args{
+				uuid:          "689c413e-a057-11f0-8de9-0242ac120002",
+				pathRecordXml: "testdata/nwbwegen222-wms.xml",
+				//categoryId:    &categoryId,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			dataToBeCreated, err := os.ReadFile(tt.args.pathRecordXml)
+			assert.Nil(t, err)
+			recordToBeCreated := string(dataToBeCreated)
+			timeString := time.Now().Format("2006-01-02 15:04:05")
+			// create unpublished record
+			recordToBeCreated = strings.Replace(recordToBeCreated, "NWB - wegen22222", "NWB - wegen33333 "+timeString, -1)
+			err = ngrClient.createOrUpdateServiceMetadataRecord(recordToBeCreated, tt.args.uuid, tt.args.categoryId, tt.args.groupId, &ngrClient.NgrConfig, false)
+			assert.Nil(t, err)
+			recordCreated, getStatus, err := ngrClient.getRecord(tt.args.uuid, &ngrClient.NgrConfig)
+			assert.Nil(t, err)
+			assert.Equal(t, http.StatusOK, getStatus)
+			assert.Contains(t, recordCreated, timeString)
+
+			// publish record and update title
+
+			//time.Sleep(60 * time.Second)
+			//dataToBeUpdated, err := os.ReadFile(tt.args.pathRecordXml)
+			//assert.Nil(t, err)
+			//recordToBeUpdated := string(dataToBeUpdated)
+			//recordToBeUpdated = strings.Replace(recordToBeUpdated, "NWB - wegen33333", "NWB - wegen00000", -1)
+			//err = ngrClient.createOrUpdateServiceMetadataRecord(recordToBeUpdated, tt.args.uuid, tt.args.categoryId, tt.args.groupId, &ngrClient.NgrConfig, false)
+			//assert.Nil(t, err)
+			//recordUpdated, getStatus, err := ngrClient.getRecord(tt.args.uuid, &ngrClient.NgrConfig)
+			//assert.Nil(t, err)
+			//assert.Equal(t, http.StatusOK, getStatus)
+			//assert.Contains(t, recordUpdated, "NWB - wegen00000")
+
+			time.Sleep(60 * time.Second)
+			deleteStatus, err := ngrClient.deleteRecord(tt.args.uuid, &ngrClient.NgrConfig)
+			assert.Nil(t, err)
+			assert.Equal(t, http.StatusNoContent, deleteStatus)
+			delRecord, getDelStatus, err := ngrClient.getRecord(tt.args.uuid, &ngrClient.NgrConfig)
+			assert.NotNil(t, err)
+			assert.Equal(t, getDelStatus, http.StatusNotFound)
+			assert.Equal(t, "", delRecord)
+		})
+	}
 }
